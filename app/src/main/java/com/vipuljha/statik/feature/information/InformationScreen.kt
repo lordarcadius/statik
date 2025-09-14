d6fe9fd5e8674056ed86c123af938a4c2f15efa2
Vipul Kumar Jha <vipuljha08@gmail.com>
2025-09-14 23:06:50 +0530

    info: Implement horizontal pager in tabs
    Signed-off-by: Vipul Kumar Jha <vipuljha08@gmail.com>

--- app/src/main/java/com/vipuljha/statik/feature/information/InformationScreen.kt
+++ app/src/main/java/com/vipuljha/statik/feature/information/InformationScreen.kt
@@ -1,7 +1,10 @@
 package com.vipuljha.statik.feature.information
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Android
 import androidx.compose.material.icons.filled.BatteryFull
@@ -11,67 +14,47 @@ import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
 import com.vipuljha.statik.core.domain.model.NavItemModel
 import com.vipuljha.statik.feature.information.tabs.AppsTab
 import com.vipuljha.statik.feature.information.tabs.BatteryTab
 import com.vipuljha.statik.feature.information.tabs.HardwareTab
 import com.vipuljha.statik.feature.information.tabs.SoftwareTab
 import com.vipuljha.statik.navigation.Route
+import kotlinx.coroutines.launch
 
 @Composable
 fun InformationScreen(modifier: Modifier = Modifier) {
-    val navController = rememberNavController()
+    val pagerState = rememberPagerState { infoTabItems.size }
+    val scope = rememberCoroutineScope()
+
+    BackHandler(enabled = pagerState.currentPage != 0) {
+        scope.launch { pagerState.animateScrollToPage(0) }
+    }
 
     Column(modifier = modifier.fillMaxSize()) {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoute = navBackStackEntry?.destination?.route
-        val selectedIndex =
-            infoTabItems.indexOfFirst { it.route::class.qualifiedName == currentRoute }
-                .coerceAtLeast(0)
-
-        ScrollableTabRow(selectedTabIndex = selectedIndex) {
+        ScrollableTabRow(selectedTabIndex = pagerState.currentPage) {
             infoTabItems.forEachIndexed { index, item ->
                 Tab(
-                    selected = currentRoute == item.route::class.qualifiedName,
-                    onClick = {
-                        navController.navigate(item.route) {
-                            popUpTo(navController.graph.startDestinationId) { inclusive = false }
-                            launchSingleTop = true
-                        }
-                    },
+                    selected = pagerState.currentPage == index,
+                    onClick = { scope.launch { pagerState.animateScrollToPage(index) } },
                     text = { Text(item.label) },
                 )
             }
         }
-        InfoTabNavGraph(navController)
-    }
-}
 
-@Composable
-private fun InfoTabNavGraph(navController: NavHostController) {
-    NavHost(
-        navController = navController,
-        startDestination = Route.InfoTabDestination.Hardware,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        composable<Route.InfoTabDestination.Hardware> {
-            HardwareTab()
-        }
-        composable<Route.InfoTabDestination.Software> {
-            SoftwareTab()
-        }
-        composable<Route.InfoTabDestination.Battery> {
-            BatteryTab()
-        }
-        composable<Route.InfoTabDestination.Apps> {
-            AppsTab()
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.fillMaxSize()
+        ) { page ->
+            when (infoTabItems[page].route) {
+                Route.InfoTabDestination.Hardware -> HardwareTab()
+                Route.InfoTabDestination.Software -> SoftwareTab()
+                Route.InfoTabDestination.Battery -> BatteryTab()
+                Route.InfoTabDestination.Apps -> AppsTab()
+                else -> Text("Page not found!")
+            }
         }
     }
 }
